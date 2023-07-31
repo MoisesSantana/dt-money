f3c99db20b944645d04f92f71e1316269b70a768
Moisés Santana <moisaant@gmail.com>
2023-07-30 23:39:00 -0300

    refactor(table): transferring responsibility from the main tag to the parent component
    The stylized component referring to the main tag was passed to the Transactions component, so its
    name was changed to TransactionsContainer

--- src/components/transactions-table/index.tsx
+++ src/components/transactions-table/index.tsx
@@ -1,62 +1,61 @@
-import { PriceHighlight, Table, TransactionsTableContainer } from './styles';
+
+import { PriceHighlight, Table } from './styles';
 
 export function TransactionsTable() {
   return (
-    <TransactionsTableContainer>
-      <Table>
-        <tbody>
-          <tr>
-            <td width='45%'>Xablau</td>
-            <td><PriceHighlight variant='income'>R$ 22.002,22</PriceHighlight></td>
-            <td>Lazer</td>
-            <td>22/02/2022</td>
-          </tr>
-          <tr>
-            <td width='45%'>Xablau</td>
-            <td><PriceHighlight variant='outcome'>- R$ 22.002,22</PriceHighlight></td>
-            <td>Lazer</td>
-            <td>22/02/2022</td>
-          </tr>
-          <tr>
-            <td width='45%'>Xablau</td>
-            <td><PriceHighlight variant='income'>R$ 22.002,22</PriceHighlight></td>
-            <td>Lazer</td>
-            <td>22/02/2022</td>
-          </tr>
-          <tr>
-            <td width='45%'>Xablau</td>
-            <td><PriceHighlight variant='outcome'>- R$ 22.002,22</PriceHighlight></td>
-            <td>Lazer</td>
-            <td>22/02/2022</td>
-          </tr>
-          <tr>
-            <td width='45%'>Xablau</td>
-            <td><PriceHighlight variant='outcome'>- R$ 22.002,22</PriceHighlight></td>
-            <td>Lazer</td>
-            <td>22/02/2022</td>
-          </tr>
-          <tr>
-            <td width='45%'>Xablau</td>
-            <td><PriceHighlight variant='income'>R$ 22.002,22</PriceHighlight></td>
-            <td>Lazer</td>
-            <td>22/02/2022</td>
-          </tr>
-          <tr>
-            <td width='45%'>Xablau</td>
-            <td>
-              <PriceHighlight variant='outcome'>- R$ 22.002,22</PriceHighlight>
-            </td>
-            <td>Lazer</td>
-            <td>22/02/2022</td>
-          </tr>
-          <tr>
-            <td width='45%'>Xablau</td>
-            <td><PriceHighlight variant='income'>R$ 22.002,22</PriceHighlight></td>
-            <td>Lazer</td>
-            <td>22/02/2022</td>
-          </tr>
-        </tbody>
-      </Table>
-    </TransactionsTableContainer>
+    <Table>
+      <tbody>
+        <tr>
+          <td width='45%'>Xablau</td>
+          <td><PriceHighlight variant='income'>R$ 22.002,22</PriceHighlight></td>
+          <td>Lazer</td>
+          <td>22/02/2022</td>
+        </tr>
+        <tr>
+          <td width='45%'>Xablau</td>
+          <td><PriceHighlight variant='outcome'>- R$ 22.002,22</PriceHighlight></td>
+          <td>Lazer</td>
+          <td>22/02/2022</td>
+        </tr>
+        <tr>
+          <td width='45%'>Xablau</td>
+          <td><PriceHighlight variant='income'>R$ 22.002,22</PriceHighlight></td>
+          <td>Lazer</td>
+          <td>22/02/2022</td>
+        </tr>
+        <tr>
+          <td width='45%'>Xablau</td>
+          <td><PriceHighlight variant='outcome'>- R$ 22.002,22</PriceHighlight></td>
+          <td>Lazer</td>
+          <td>22/02/2022</td>
+        </tr>
+        <tr>
+          <td width='45%'>Xablau</td>
+          <td><PriceHighlight variant='outcome'>- R$ 22.002,22</PriceHighlight></td>
+          <td>Lazer</td>
+          <td>22/02/2022</td>
+        </tr>
+        <tr>
+          <td width='45%'>Xablau</td>
+          <td><PriceHighlight variant='income'>R$ 22.002,22</PriceHighlight></td>
+          <td>Lazer</td>
+          <td>22/02/2022</td>
+        </tr>
+        <tr>
+          <td width='45%'>Xablau</td>
+          <td>
+            <PriceHighlight variant='outcome'>- R$ 22.002,22</PriceHighlight>
+          </td>
+          <td>Lazer</td>
+          <td>22/02/2022</td>
+        </tr>
+        <tr>
+          <td width='45%'>Xablau</td>
+          <td><PriceHighlight variant='income'>R$ 22.002,22</PriceHighlight></td>
+          <td>Lazer</td>
+          <td>22/02/2022</td>
+        </tr>
+      </tbody>
+    </Table>
   );
 }
