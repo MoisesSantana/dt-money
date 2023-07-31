bdab01f454fc8863259cc9fed8eb796553f60cd7
Moisés Santana <moisaant@gmail.com>
2023-07-30 23:40:22 -0300

    feat(search-form): prototyping the SearchForm component

--- src/pages/transactions/index.tsx
+++ src/pages/transactions/index.tsx
@@ -1,13 +1,19 @@
 import { Header } from '@/components/header';
+import { SearchForm } from '@/components/search-form';
 import { Summary } from '@/components/summary';
 import { TransactionsTable } from '@/components/transactions-table';
+
+import { TransactionsContainer } from './styles';
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
-      <TransactionsTable />
+      <TransactionsContainer>
+        <SearchForm />
+        <TransactionsTable />
+      </TransactionsContainer>
     </div>
   );
 }
