7e603b10de9147d90533b07ab827ff89b0bf89af
Moisés Santana <moisaant@gmail.com>
2023-08-07 02:35:52 -0300

    feat(summary): consume context
    The summary component now consumes transactions from the context api, now its information is
    dynamic.

--- src/components/summary/index.tsx
+++ src/components/summary/index.tsx
@@ -1,8 +1,25 @@
+import { useContext } from 'react';
+
 import { SummaryCard, SummaryContainer } from './styles';
 
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react';
 
+import { TransactionsContext } from '../../contexts/TransactionsContext';
+
 export function Summary() {
+  const { transactions } = useContext(TransactionsContext);
+
+  const summary = transactions.reduce((acc, transaction) => {
+    if (transaction.type === 'income') {
+      acc.income += transaction.price;
+      acc.total += transaction.price;
+    } else {
+      acc.outcome += transaction.price;
+      acc.total -= transaction.price;
+    }
+    return acc;
+  }, { income: 0, outcome: 0, total: 0 });
+  console.log(summary);
   return (
     <SummaryContainer>
       <SummaryCard>
@@ -10,21 +27,30 @@ export function Summary() {
           <span>Entradas</span>
           <ArrowCircleUp size={32} color='#00b37e' />
         </header>
-        <strong>R$ 22.002,22</strong>
+        <strong>
+          R$
+          {summary.income}
+        </strong>
       </SummaryCard>
       <SummaryCard>
         <header>
           <span>Saídas</span>
           <ArrowCircleDown size={32} color='#f75a68' />
         </header>
-        <strong>R$ 22.002,22</strong>
+        <strong>
+          R$
+          {summary.outcome}
+        </strong>
       </SummaryCard>
       <SummaryCard variant='green'>
         <header>
           <span>Total</span>
           <CurrencyDollar size={32} color='#fff' />
         </header>
-        <strong>R$ 22.002,22</strong>
+        <strong>
+          R$
+          {summary.total}
+        </strong>
       </SummaryCard>
     </SummaryContainer>
   );
